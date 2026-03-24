858299823b0cf549d909a4a986f188cb9dad3ab0
Richard Santos <richardd.ferreira06@gmail.com>
2026-03-23 23:13:08 -0300

    feat: implementa domain e model do seller

--- gest-stock/src/Domain/user.py
+++ gest-stock/src/Domain/user.py
@@ -1,12 +1,19 @@
 class UserDomain:
-    def __init__(self, id, name, email, password):
+    def __init__(self, id, name, cnpj, email, phone, password, status="INACTIVE"):
         self.id = id
         self.name = name
-        self.email = email        
-    
+        self.cnpj = cnpj
+        self.email = email
+        self.phone = phone
+        self.password = password
+        self.status = status
+ 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
-            "email": self.email,            
+            "cnpj": self.cnpj,
+            "email": self.email,
+            "phone": self.phone,
+            "status": self.status
         }
