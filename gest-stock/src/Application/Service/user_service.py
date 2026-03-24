5426d969362c20c208334684cda658f7d9546a21
Geovane Soares da Silva <geovanesilva@MacBook-Air-de-Geovane.local>
2026-03-24 00:10:31 -0300

    feat: atualiza service e requirements do seller

--- gest-stock/src/Application/Service/user_service.py
+++ gest-stock/src/Application/Service/user_service.py
@@ -1,11 +1,36 @@
 from src.Domain.user import UserDomain
 from src.Infrastructure.Model.user import User
-from src.config.data_base import db 
+from src.config.data_base import db
 
 class UserService:
     @staticmethod
-    def create_user(name, email, password):        
-        user = User(name=name, email=email, password=password)        
+    def create_user(name, cnpj, email, phone, password):
+
+        # Verifica duplicidade antes de tentar salvar
+        if User.query.filter_by(email=email).first():
+            raise ValueError("E-mail já cadastrado")
+
+        if User.query.filter_by(cnpj=cnpj).first():
+            raise ValueError("CNPJ já cadastrado")
+
+        user = User(
+            name=name,
+            cnpj=cnpj,
+            email=email,
+            phone=phone,
+            status="INACTIVE"
+        )
+        user.set_password(password)  # hash da senha
+
         db.session.add(user)
-        db.session.commit()       
-        return UserDomain(user.id, user.name, user.email, user.password)
+        db.session.commit()
+
+        return UserDomain(
+            id=user.id,
+            name=user.name,
+            cnpj=user.cnpj,
+            email=user.email,
+            phone=user.phone,
+            password=user.password,
+            status=user.status
+        )
